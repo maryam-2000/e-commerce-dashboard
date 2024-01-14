4d2d62b8a65210c171a6e8bdd6cf36e062e196c9
Maryam <maryammagdy164@gmail.com>
2024-01-15 00:49:36 +0200

    Final commit

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -8,19 +8,8 @@ import Cart from './Pages/Cart';
 import Footer from './Components/Footer/Footer';
 import LoginSignup from './Pages/LoginSignup';
 import Admin from './Pages/Admin.jsx';
-// const config = require('./config.js');
 
 function App() {
-  // if(config.isFirstTimeStartup){
-  //   localStorage.removeItem('auth-token');
-
-  //   config.isFirstTimeStartup = false;
-  //   flag = false;
-  // }
-  // console.log(localStorage.getItem('auth-token'));
-  // console.log(localStorage.getItem('customerId'));
-  // console.log(localStorage.getItem('customerName'));
-  // console.log(localStorage.getItem('isAdmin'));
   return (
     <div>
       <BrowserRouter>
